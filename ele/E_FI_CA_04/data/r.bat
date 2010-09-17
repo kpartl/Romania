303a38c65c0422f6da8584463274822970288199
unknown <K14427@.in.jme.cz>
2010-09-17 13:26:29 +0200

    x

---
+++ ele/E_FI_CA_04/data/r.bat
@@ -0,0 +1,5 @@
+copy c:\ManagementGateway\1.0\root\applications\TEST\tmp\function_file\*.* c:\ManagementGateway\1.0\root\applications\TEST\data\function_file\
+copy c:\ManagementGateway\1.0\root\applications\TEST\tmp\description_file\*.* c:\ManagementGateway\1.0\root\applications\TEST\data\description_file\
+copy c:\ManagementGateway\1.0\root\applications\TEST\tmp\platform.dua  c:\ManagementGateway\1.0\root\applications\TEST\Development\exported_platforms\
+del c:\ManagementGateway\1.0\root\applications\TEST\Development\out\out.pdf
+copy c:\projects\strs\Rom\ele\E_FI_CA_04\data\FI-CA_04_ELE_-_test1.txt c:\ManagementGateway\1.0\root\applications\TEST\Development\in\
